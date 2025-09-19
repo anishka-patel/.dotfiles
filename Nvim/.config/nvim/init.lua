853fc0f523e7d704e127afdbd9b94efdbe926e0e
Anishka Patel <anishka.vpatel@gmail.com>
2025-09-19 08:03:25 +0530

    Minor Tweak

--- Nvim/.config/nvim/init.lua
+++ Nvim/.config/nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.vscode then
+  local keymap = vim.keymap.set
+  keymap("n", "<leader>", "<cmd>call VSCodeNotify('whichkey.show')<CR>", opts)
+  keymap("x", "<leader>", "<cmd>call VSCodeNotify('whichkey.show')<CR>", opts)
+end
